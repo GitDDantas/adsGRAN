b9b7029ef235e0c0e101ff17c3fadbb7506c38da
GitDDantas <oidanieldantas+github@gmail.com>
2023-06-21 06:18:37 -0300

    HTML e JS - pegar qtd de itens.

--- projetoLojinha/js/funcoesCarrinho.js
+++ projetoLojinha/js/funcoesCarrinho.js
@@ -1,14 +1,12 @@
 function alterarQtd(adicRemo, ProdN) {
-  let qtd = null;
+  let qtd = parseInt(document.getElementById('qtd'+ProdN).innerHTML);
   switch (adicRemo) {
     case "adicionar":
-      qtd = parseInt(document.getElementById('qtd'+ProdN).innerHTML);
       qtd+=1;
       document.getElementById('qtd'+ProdN).innerHTML = qtd;      
       break;
 
     case "remover":
-      qtd = parseInt(document.getElementById('qtd'+ProdN).innerHTML);
       qtd > 0 ? qtd-=1 : 0;
       document.getElementById('qtd'+ProdN).innerHTML = qtd;
       break;
@@ -25,8 +23,9 @@ function alterarQtd(adicRemo, ProdN) {
 }
 
 function totalizar() {
+    const qtdItens = parseInt(document.getElementById('qtdItens').innerHTML);
     let total = 0;
-    for(let i = 1; i <= 3; i++){
+    for(let i = 1; i <= qtdItens; i++){
         total += parseFloat((document.getElementById('vTotalP0'+i).innerHTML).replace(/\,/g,"."))
     }
     document.getElementById('subTotal').innerHTML=total.toFixed(2).replace(/\./g,",");
