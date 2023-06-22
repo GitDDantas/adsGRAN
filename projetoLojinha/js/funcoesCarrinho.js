5ec5b26d049972a67448fd6294b49a1311367993
GitDDantas <oidanieldantas+github@gmail.com>
2023-06-21 21:43:11 -0300

    html pt-BR

--- projetoLojinha/js/funcoesCarrinho.js
+++ projetoLojinha/js/funcoesCarrinho.js
@@ -3,19 +3,19 @@ function alterarQtd(adicRemo, ProdN) {
   switch (adicRemo) {
     case "adicionar":
       qtd+=1;
-      document.getElementById('qtd'+ProdN).innerHTML = qtd;      
+      //document.getElementById('qtd'+ProdN).innerHTML = qtd;      
       break;
 
     case "remover":
       qtd > 0 ? qtd-=1 : 0;
-      document.getElementById('qtd'+ProdN).innerHTML = qtd;
+      //document.getElementById('qtd'+ProdN).innerHTML = qtd;
       break;
 
     default:
         alert("Opção inválida. Somente: adicionar ou remover");
       break;
   }
-
+  document.getElementById('qtd'+ProdN).innerHTML = qtd;
   const vTotalProdN = (document.getElementById('prc'+ProdN).innerHTML.replace(/\,/g,".")*qtd).toFixed(2);
   document.getElementById('vTotal'+ProdN).innerHTML = vTotalProdN.replace(/\./g,",");
   totalizar();
